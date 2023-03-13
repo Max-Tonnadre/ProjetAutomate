58b9cb06ab77da442e83fd23a368aec75c82f96e
MarleneSchiappaa <max.tonnadre@efrei.net>
2023-03-13 17:27:32 +0100

    ++

--- automate.py
+++ automate.py
@@ -1,11 +1,9 @@
 def AddAutomate():
-    f=open("automate.txt","a")
-    f.write(";\n")
-    finis=0
+    f=open("automate.txt","w")
     Etats=input("Entrer les diffrents Etats de l'automate :").split(",")
     transitions=input("Entrer les diffentes transitions de l'alphabet :").split(",")
     for etat in Etats:
-        entresortie=input("Si Etat initial,entrer 1,si Etat final,entrer 2,si Etat initial et final,entrer 3,si Etat normal,entrer 4 :")
+        entresortie=input("Si Etat "+etat+" initial,entrer 1,si Etat final,entrer 2,si Etat initial et final,entrer 3,si Etat normal,entrer 4 :")
         f.write(entresortie+"*"+etat+":")
         for e in transitions:
             f.write("(")
@@ -14,7 +12,6 @@ def AddAutomate():
             f.write(e+","+etatSuivant)
             f.write(')')
         f.write("\n")
-    f.write(";")
     f.close()
 
 
@@ -22,10 +19,19 @@ def AddAutomate():
 
 
 
-def displayAutomate():
+def displayTableAutomate():
     f=open("automate.txt","r")
-    print(f.read())
+    lines=f.readlines()
+    for line in lines:
+        print("---------------------")
+        if(line[0]=="1"):
+            print("E |"+line[2])
+        if(line[0]=="2"):
+            print("F |"+line[2])
+        if(line[0]=="3"):
+            print("EF|"+line[2])
+        if(line[0]=="  |4"):
+            print(line[2])
     f.close()
 
-
-AddAutomate()
+displayTableAutomate()
