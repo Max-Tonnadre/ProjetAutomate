72460a4f733c634975fe64afbc64263a80f72d45
RaphaelC1 <100593405+RaphaelC1@users.noreply.github.com>
2023-03-26 20:06:22 +0200

    menu

---
+++ menu.py
@@ -0,0 +1,94 @@
+from automate import *
+
+def startMenu():
+    print("|-------------Bienvenue--------------|")
+    print("|                                    |")
+    print("|     Que souhaitez-vous faire ?     |")
+    print("|                                    |")
+    print("|       1. Voir un automate          |")
+    print("|                                    |")
+    print("|       2. Sortie                    |")
+    print("|------------------------------------|\n")
+
+def controlStartMenu():
+    n = controlInput(1,2)
+    if (n==1):
+        menuAutomate()
+        controlAutomate()
+    else:
+        exit()
+
+
+def menuAutomate():
+    print("|---------Voir un automate-----------|")
+    print("|                                    |")
+    print("|Choissisez un automate parmis les 44|")
+    print("|                                    |")
+    print("|------------------------------------|\n")
+
+
+def controlAutomate():
+    global numAutomate
+    numAutomate = controlInput(1,44)
+    menuAutomate2()
+    controlAutomate2()
+    s = str(numAutomate)
+    with open(s + ".txt", "r") as f:
+        contenu = f.read()
+        print(contenu)
+
+
+
+def menuAutomate2():
+    print("|-----Que souhaitez-vous faire ?-----|")
+    print("|                                    |")
+    print("|       1. Voir son type             |")
+    print("|                                    |")
+    print("|       2. Standardiser l'automate   |")
+    print("|                                    |")
+    print("|       3. Déterminiser l'automate   |")
+    print("|                                    |")
+    print("|       4. Retour                    |")
+    print("|------------------------------------|\n")
+
+def controlAutomate2():
+    n= controlInput(1,4)
+    if (n==1):
+        typeAutomate()
+        print("")
+        menuAutomate2()
+        controlAutomate2()
+    elif (n==2):
+        Standardisation()
+        print("")
+        menuAutomate2()
+        controlAutomate2()
+    elif (n==3):
+        Determinisation()
+        print("")
+        menuAutomate2()
+        controlAutomate2()
+    elif (n == 4):
+        startMenu()
+        controlStartMenu()
+
+
+def typeAutomate():
+    if isStandard() == True:
+        print("L'automate est standard")
+    else:
+        print("L'automate n'est pas standard")
+    if isDeterminist()==True:
+        print("L'automate est déterministe")
+    else:
+        print("L'automate n'est pas déterministe")
+
+
+def controlInput(min_val, max_val):
+    n = int(input("Votre choix : "))
+
+    while (n < min_val or n > max_val):
+        print("Erreur, veuillez ressaisir : ")
+        n = int(input())
+
+    return n
